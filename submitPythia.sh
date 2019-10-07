4365f92f21397a8bc15668a246c7dd26ca8a77ee
Oliver Matonoha <oliver.matonoha@centrum.cz>
2019-10-07 20:12:54 +0200

    submitter

---
+++ submitPythia.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo "How many processes?"
+read npr
+echo "How many events per process?"
+read nev
+for ((i = 0 ; i < $npr ; i++)); do
+	make makeTreeSoRt; ./makeTreeSoRt.exe $nev "output_${i}.root" 0 > "output_${i}.log" &
+	echo "finished " ${i}
+    pids[${i}]=$!
+done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
+echo "Finished with pythia jobs, merging now"
+touch output.log
+for ((i = 0 ; i < $npr ; i++)); do
+	echo "output_${i}.log" >> output.log 
+	cat output_${i}.log >> output.log
+	rm output_${i}.log
+done
+
+#hadd output.root output_*.root
+#rm output_*.root
